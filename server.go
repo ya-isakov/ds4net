457a9c3a8a8c0b09a557ebd46e25ddbda2dc41a0
Yaroslav Isakov <yaroslav.isakov@gmail.com>
2018-03-31 20:45:25 +0200

    Add sticks for Go prototype

--- server.go
+++ server.go
@@ -22,6 +22,10 @@ type Packet struct {
 var events int = 0
 var clients map[*net.UDPAddr]bool = make(map[*net.UDPAddr]bool, 0)
 
+var dpads_map = map[byte]uint16{0: 0x1, 1: 0x9, 2: 0x8, 3: 0xA, 4: 0x2, 5: 0x6, 6: 0x4, 7: 0x5}
+var buttons_map = map[int]uint16{4: 0x4000, 5: 0x1000, 6: 0x2000, 7: 0x8000}
+var misc_map = map[int]uint16{0: 0x100, 1: 0x200, 4: 0x20, 5: 0x10, 6: 0x40, 7: 0x80}
+
 func get_bit(data []byte, num int, bit uint) bool {
 	return (data[num] & (1 << bit)) != 0
 }
@@ -38,11 +42,35 @@ func to_big(data uint16) []byte {
 	return []byte{byte(data % 256), byte(data >> 8)}
 }
 
-func parse_and_map(hid []byte) []byte {
-	dpads_map := map[byte]uint16{0: 0x1, 1: 0x9, 2: 0x8, 3: 0xA, 4: 0x2, 5: 0x6, 6: 0x4, 7: 0x5}
-	buttons_map := map[int]uint16{4: 0x4000, 5: 0x1000, 6: 0x2000, 7: 0x8000}
-	misc_map := map[int]uint16{0: 0x100, 1: 0x200, 4: 0x20, 5: 0x10, 6: 0x40, 7: 0x80}
+func clamp(v int32) int32 {
+	min := int32(32767)
+	if v < 32767 {
+		min = v
+	}
+	max := min
+	if max < -32767 {
+		max = -32767
+	}
+	return max
+}
 
+func correct_axis(v, min, max, dz int32) int32 {
+	t1 := (max + min) / 2
+	c0 := t1 - dz
+	c1 := t1 + dz
+	t2 := (max - min - 4*dz) / 2
+	c2 := (1 << 29) / t2
+	r0 := (c2 * (v - c0)) >> 14
+	r1 := (c2 * (v - c1)) >> 14
+	if v < c0 {
+		return clamp(r0)
+	} else if v > c1 {
+		return clamp(r1)
+	}
+	return 0
+}
+
+func parse_and_map(hid []byte) []byte {
 	buttons := dpads_map[hid[5]%16]
 	for k, v := range buttons_map {
 		buttons = set_if_true(buttons, v, get_bit(hid, 5, uint(k)))
@@ -53,14 +81,13 @@ func parse_and_map(hid []byte) []byte {
 	l2_analog := byte(hid[8])
 	r2_analog := byte(hid[9])
 
-	if buttons > 0 {
-		fmt.Printf("%v %v %v\n", buttons, l2_analog, r2_analog)
-	}
-
 	ret := []byte{1}
 	ret = append(ret, to_big(buttons)...)
 	ret = append(ret, l2_analog)
 	ret = append(ret, r2_analog)
+	for _, axis := range hid[1:5] {
+		ret = append(ret, to_big(uint16(correct_axis(int32(axis), 0, 255, 5)))...)
+	}
 	return ret
 }
 
@@ -78,7 +105,6 @@ func reader(f *os.File, conn *net.UDPConn) {
 		events = events + 1
 		for client := range clients {
 			conn.WriteTo(packet, client)
-			//fmt.Printf("Client %v %v\n", client, count)
 		}
 	}
 }
@@ -116,11 +142,11 @@ func main() {
 	go reader(file, conn)
 	//go outputter()
 	for {
-		_, addr, err := conn.ReadFromUDP(buf)
+		n, addr, err := conn.ReadFromUDP(buf)
 		if err != nil {
 			log.Fatal(err)
 		}
-		if buf[0] == 0 {
+		if buf[0] == 0 && n == 1 {
 			fmt.Printf("Gotcha %v\n", addr)
 			clients[addr] = true
 		}
