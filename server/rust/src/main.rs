984f579d69482179b93341182df94aa39d902ed5
Yaroslav Isakov <yaroslav.isakov@gmail.com>
2018-12-10 16:33:27 +0100

    First try of rumble

--- server/rust/src/main.rs
+++ server/rust/src/main.rs
@@ -3,22 +3,27 @@ use std::fs::File;
 use std::io::prelude::*;
 use std::net::SocketAddr;
 use std::net::UdpSocket;
-use std::sync::{Arc, RwLock};
+use std::sync::Arc;
 use std::thread;
 
 extern crate crossbeam_channel;
 use crossbeam_channel::{unbounded, Receiver, Sender};
 
+extern crate parking_lot;
+use parking_lot::RwLock;
+
 extern crate crc;
 use crc::crc32;
+
+const DEFAULT_LATENCY: u8 = 4;
 
 fn handle_new_client(addr: SocketAddr, r: Receiver<[u8; 77]>) {
     println!("addr: {}", addr);
     let client = UdpSocket::bind("0.0.0.0:0").unwrap();
     client.connect(addr).unwrap();
-    let mut connected = false;
+    let mut connected: bool = false;
     loop {
-        let packet = r.recv().unwrap();
+        let packet: [u8; 77] = r.recv().unwrap();
         match client.send(&packet) {
             Ok(_) => true,
             Err(ref err) if (err.kind() == std::io::ErrorKind::ConnectionRefused && connected) => {
@@ -34,21 +39,68 @@ fn handle_new_client(addr: SocketAddr, r: Receiver<[u8; 77]>) {
     }
 }
 
-fn handle_disconnect(addr: SocketAddr, _clients: &mut Vec<SocketAddr>) {}
+//fn handle_disconnect(addr: SocketAddr, _clients: &mut Vec<SocketAddr>) {}
 
-fn handle_rumble() {}
+fn get_control(_val: &str) -> u8 {
+    0
+}
+
+fn transform_u32_to_array_of_u8(x: u32) -> [u8; 4] {
+    let b0: u8 = ((x >> 24) & 0xff) as u8;
+    let b1: u8 = ((x >> 16) & 0xff) as u8;
+    let b2: u8 = ((x >> 8) & 0xff) as u8;
+    let b3: u8 = (x & 0xff) as u8;
+    return [b3, b2, b1, b0];
+}
+
+fn fill_packet() -> [u8; 78] {
+    let mut pkt: [u8; 78] = [0; 78];
+    pkt[0] = 0x11;
+    pkt[1] = 0xC0 | DEFAULT_LATENCY;
+    pkt[3] = 0x07;
+    pkt[6] = get_control("small_rumble");
+    pkt[7] = get_control("big_rumble");
+    pkt[8] = get_control("r");
+    pkt[9] = get_control("g");
+    pkt[10] = get_control("b");
+    // Time to flash bright (255 = 2.5 seconds)
+    pkt[11] = 0; // min(flash_led1, 255)
+    // Time to flash dark (255 = 2.5 seconds)
+    pkt[12] = 0; // min(flash_led2, 255)
+    pkt[21] = get_control("volume_l");
+    pkt[22] = get_control("volume_r");
+    pkt[23] = 0x49; // magic
+    pkt[24] = get_control("volume_speaker");
+    pkt[25] = 0x85; //magic
+    pkt
+}
+
+fn checksum(packet: &[u8]) -> [u8; 4] {
+    let mut full_packet: [u8; 75] = [0; 75];
+    full_packet[0] = 0xA2;
+    full_packet[1..].copy_from_slice(packet);
+    let hasher = crc32::checksum_ieee(&full_packet);
+    transform_u32_to_array_of_u8(hasher.to_le())
+}
+
+fn handle_rumble() {
+    let mut pkt = fill_packet();
+    let crc = checksum(&pkt[0..74]);
+    pkt[74..78].copy_from_slice(&crc);
+    println!("{:?}", &pkt[..]);
+}
 
 fn handle_udp(clients: Arc<RwLock<HashMap<SocketAddr, Sender<[u8; 77]>>>>) {
     let socket = UdpSocket::bind("127.0.0.1:9999").unwrap();
-    let mut buf = [0; 4];
+    let mut buf: [u8; 4] = [0; 4];
     loop {
         let (amt, src) = socket.recv_from(&mut buf).unwrap();
         let buf = &mut buf[..amt];
         match buf[0] {
             0 => {
                 let (s, r) = unbounded();
-                clients.write().unwrap().insert(src, s);
-                let handle = thread::spawn(move || handle_new_client(src, r));
+                clients.write().insert(src, s);
+                let _handle = thread::spawn(move || handle_new_client(src, r));
             }
             1 => handle_rumble(),
             _ => panic!("Bohuzel"),
@@ -57,12 +109,10 @@ fn handle_udp(clients: Arc<RwLock<HashMap<SocketAddr, Sender<[u8; 77]>>>>) {
 }
 
 fn main() {
-    //is_sync::<*mut Vec<std::net::SocketAddr>>();
-    /*let hasher = crc32::checksum_ieee(b"1234");
-    println!("hash {}", hasher.to_be());*/
-    let mut clients = Arc::new(RwLock::new(HashMap::new()));
+    handle_rumble();
+    let clients = Arc::new(RwLock::new(HashMap::new()));
     let c_clients = Arc::clone(&clients);
-    let handle = thread::spawn(move || {
+    let _handle = thread::spawn(move || {
         handle_udp(c_clients);
     });
     let mut f = File::open("/dev/hidraw2").unwrap();
@@ -72,9 +122,9 @@ fn main() {
         let count = f.read(&mut packet).unwrap();
         assert_eq!(count, 77);
         assert_eq!(packet[0], 0x11);
-        for (addr, s) in c_clients.read().unwrap().iter() {
+        for (_addr, s) in c_clients.read().iter() {
             s.send(packet).unwrap()
         }
     }
-    handle.join().unwrap();
+    //handle.join().unwrap();
 }
